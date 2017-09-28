947b12b0305c5fe37385c1399bb6e78e96570c0b
SrishtiPalani <srishti.palani@gmail.com>
2017-09-27 20:30:12 -0400

    my additions

--- textsort.c
+++ textsort.c
@@ -36,5 +36,42 @@ int main( int argc, char ** argv)
 	fprintf(stderr, "Error: Empty file. Please check. %s\n", input_file);
 	exit(1);
 	}
+
+	char **text = (char **) malloc(sizeof(char*) * lineNum);		//allocating a pointer to pointers of char - will be used as
+																					//an array of text with one entry per line of text
+	if (text == NULL)
+	{
+		fprintf(stderr, "Memory Allocation failed.\n");
+		exit(1);
+	}
+
+	int lines =0;
+	int ch =0; 
+	lines++; 
+	while ((ch = fgetc(in_file)) != EOF)
+    {
+      if (ch == '\n')
+    lines = lines+ 1;
+    }
+    printf("There are %d lines in this file\n", lines);
+
+
+	for (int line = 0; line < lines; line++) {
+	 	 int lineLength = 0;			
+
+		if (line >= lineNum)//we've run out the memory that we allocated so we double our allocated memory
+		{
+			int morelineNum = lineNum * 2;						
+																						
+			text = (char **) realloc(text, sizeof(char*) * morelineNum);
+
+			if (text == NULL)
+			{
+				fprintf(stderr, "Memory Allocation for Text failed.\n");
+				exit(1);
+			}
+
+			lineNum = morelineNum;
+		}
 return 0;
 }
